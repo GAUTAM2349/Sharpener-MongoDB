f069bc80a52cce80608fcd2b2f4e69e06f74019f
gautamguddu <gautamkumarpandey2526@gmail.com>
2025-06-30 00:19:24 +0530

    added order functionalities

--- routes/cart.js
+++ routes/cart.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const cartController = require('../controllers/cart');
 const authenticate = require('../middlewares/usersOnly');
 
-// All routes are protected
+
 router.get('/', authenticate, cartController.getCart);
 router.post('/add', authenticate, cartController.addToCart);
 router.delete('/remove/:productId', authenticate, cartController.removeFromCart);
